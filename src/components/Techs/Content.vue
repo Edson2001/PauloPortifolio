d00496881867f6bf15e841a7d97b91290d672676
edson2001 <djiedson413@gmai.com>
2022-11-20 19:12:16 +0100

    techs

--- src/components/Techs/Content.vue
+++ src/components/Techs/Content.vue
@@ -1,33 +1,101 @@
+<script lang="ts" setup>
+
+    const used = [
+        {
+            img: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/svelte/svelte-original.svg'
+        },
+        {
+            img: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/storybook/storybook-original.svg'
+        },
+        {
+            img: '/images/Vector(2).svg'
+        },
+        {
+            img: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/spring/spring-original.svg'
+        },
+        {
+            img: '/images/Vector(1).svg'
+        },
+        {
+            img: '/images/Vector(2).svg'
+        },
+        {
+            img: '/images/Vector.svg'
+        },
+        {
+            img: '/images/Vector(1).svg'
+        },
+        {
+            img: '/images/Vector(2).svg'
+        },
+        {
+            img: '/images/Vector.svg'
+        },
+        {
+            img: '/images/Vector(1).svg'
+        },
+        {
+            img: '/images/Vector(2).svg'
+        }
+    ]   
+
+    const studying = [
+        {
+            img: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/svelte/svelte-original.svg'
+        },
+        {
+            img: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/storybook/storybook-original.svg'
+        },
+        {
+            img: '/images/Vector(2).svg'
+        },
+        {
+            img: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/spring/spring-original.svg'
+        },
+        {
+            img: '/images/Vector(1).svg'
+        },
+        {
+            img: '/images/Vector(2).svg'
+        },
+        {
+            img: '/images/Vector.svg'
+        },
+        {
+            img: '/images/Vector(1).svg'
+        },
+        {
+            img: '/images/Vector(2).svg'
+        },
+        {
+            img: '/images/Vector.svg'
+        },
+        {
+            img: '/images/Vector(1).svg'
+        },
+        {
+            img: '/images/Vector(2).svg'
+        }
+    ]
+
+</script>
+
 <template >
     <section class="techsSection">
         <h2><span>Minhas</span> Techs</h2>
         <p>Tecnologias</p>
         <p class="techsUsed">Já <span>utilizadas</span></p>
+        
         <ul>
-            <li>
-                <img src="/images/Vector.svg" alt="">
-            </li>
-            <li>
-                <img src="/images/Vector(1).svg" alt="">
-            </li>
-            <li>
-                <img src="/images/Vector(2).svg" alt="">
+            <li v-for="(item, index) in used" :key="index" >
+                <img :src="item.img" alt="">
             </li>
         </ul>
 
         <p class="techsUsed">Atualmente  <span>Desenvolvendo</span></p>
         <ul>
-            <li>
-                <img src="/images/used/Vector.svg" alt="">
-            </li>
-            <li>
-                <img src="/images/used/Vector(1).svg" alt="">
-            </li>
-            <li>
-                <img src="/images/used/Group14.svg" alt="">
-            </li>
-            <li>
-                <img src="/images/used/Group16.svg" alt="">
+            <li v-for="(item, index) in studying" :key="index">
+                <img :src="item.img" alt="">
             </li>
         </ul>
     </section>
